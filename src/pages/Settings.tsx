aa6cd3d6d904a72ad039d2701e4e76be4671d233
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2024-05-07 21:11:40 +1000

    add access logs

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,32 +1,26 @@
 import { deleteUser, sendEmailVerification, updateProfile } from 'firebase/auth';
 import { createStore } from "solid-js/store";
 import { A, useNavigate } from '@solidjs/router';
-import { For, createEffect, onMount } from 'solid-js';
+import { For, onMount } from 'solid-js';
 
 import { userSignal } from '../common/userSignal';
-import { UserSession } from '../../dataTypes';
+import { User, UserSession } from '../../dataTypes';
+import { fetchAuthUserGet } from '../serviceAuth/authUser';
 import { fetchAuthUserSessionGet } from '../serviceAuth/authUserSession';
 
 type SettingsStore = {
-  user: {
-    uid: string | undefined;
-    name: string | undefined;
-    email: string | undefined;
-    emailVerified: boolean | undefined;
-  }
+  user: User | undefined;
   userSessions: UserSession[] | undefined;
+
+  searchQuery: string | undefined;
 }
 
 const defaultSettingsStore: SettingsStore = {
-  user: {
-    uid: undefined,
-    name: undefined,
-    email: undefined,
-    emailVerified: undefined,
-  },
+  user: undefined,
   userSessions: undefined,
-}
 
+  searchQuery: "",
+}
 
 export default function Settings() {
   const navigate = useNavigate();
@@ -45,8 +39,21 @@ export default function Settings() {
     });
   }
 
+  const sendVerificationEmail = () => {
+    if (window.confirm("Do you want to resend the verification email?")) {
+      sendEmailVerification(userSignal())
+        .then(() => {
+          alert("Verification email sent")
+        })
+        .catch((err) => {
+          console.error(err)
+          alert("An error occurred. Please try again.")
+        });
+    }
+  }
+
   const deleteAccount = () => {
-    if (window.confirm("Are you sure you want to delete your account?")) {
+    if (window.confirm("Are you sure you want to delete your account? This action is irreversible.")) {
       deleteUser(userSignal())
         .then(() => {
           console.log("Account deleted")
@@ -63,37 +70,38 @@ export default function Settings() {
     const userSession = await fetchAuthUserSessionGet(uid);
     setConfigStore('userSessions', userSession);
   }
-  
-  createEffect(() => {
-    setConfigStore('user', {
-      uid: userSignal().uid,
-      name: userSignal().displayName,
-      email: userSignal().email,
-      emailVerified: userSignal().emailVerified,
-    });
+
+  const filteredUserSessions = () => {
+    return configStore.searchQuery
+      ? configStore.userSessions.filter(session =>
+        new Date(session.login_at).getDate() === new Date(configStore.searchQuery).getDate()
+        || (session.logout_at && new Date(session.logout_at).getDate() === new Date(configStore.searchQuery).getDate())
+      )
+      : configStore.userSessions;
+  }
+
+  onMount(async () => {
+    const user = await fetchAuthUserGet();
+    setConfigStore('user', user);
+
     fetchUserSessions(configStore.user.uid);
   });
 
   return (
     <div
-      class=""
+      class="flex flex-col items-center justify-center"
     >
       <div
-        class="flex flex-col pt-5 px-5 w-full items-center"
+        class="flex flex-col p-5 w-full items-center"
       >
         <div 
-          class="flex flex-col items-center justify-center w-1/2"
+          class="flex flex-col items-center justify-center w-full max-w-2xl"
         >
           <h1
-            class="text-3xl font-display font-bold"
+            class="text-3xl font-bold"
           >
             Settings
           </h1>
-          <p
-            class="text-sm mt-2"
-          >
-            Manage your account settings.
-          </p>
           <div
             class="flex flex-col px-5 py-2 space-y-2 rounded-lg mt-5 w-full"
           >
@@ -108,7 +116,7 @@ export default function Settings() {
               <input
                 type="text"
                 class="border border-gray-300 rounded-md w-full px-2"
-                value={configStore.user.name}
+                value={configStore.user && configStore.user.name}
                 onInput={(e) => setConfigStore(prev => ({ ...prev, name: e.currentTarget.value }))}
               />
             </label>
@@ -123,7 +131,7 @@ export default function Settings() {
               <input
                 type="email"
                 class="border border-gray-300 rounded-md w-full px-2"
-                value={configStore.user.email}
+                value={configStore.user && configStore.user.email}
                 onInput={(e) => setConfigStore(prev => ({ ...prev, email: e.currentTarget.value }))}
                 disabled
               />
@@ -133,102 +141,137 @@ export default function Settings() {
             class="flex flex-col items-center justify-center w-full py-2"
           >
             <button
-              class="flex items-center justify-center rounded-full text-white font-display bg-green-500 w-full my-2 py-2 disabled:bg-green-300 hover:bg-green-600 transition duration-300"
+              class="flex items-center justify-center rounded-full text-white bg-indigo-500 w-1/2 mt-4 py-3 disabled:bg-indigo-300 hover:bg-indigo-600 transition duration-300"
               onClick={updateUserProfile}
             >
               Save
             </button>
           </div>
           <div
-            class="flex space-x-2 items-center justify-center w-full"
+            class="flex space-x-2 items-center justify-center w-full pt-2 pb-4"
           >
-            <A href="/forgot"
-              class="text-sm mt-2 underline"
+            <A
+              href="/forgot"
+              class="text-sm text-indigo-500 hover:underline transition duration-300"
             >
               Change password
             </A>
-            {!configStore.user.emailVerified &&
-              <p
-                onClick={() => sendEmailVerification(userSignal()).then(() => alert("Verification email sent"))}
-                class="text-sm mt-2 underline"
-              >
-                Resend verification email
-              </p>
-            }
-          </div>
-        </div>
-        <div
-          class="flex flex-col items-end justify-center w-1/2"
-        >
-          <div
-            class="flex flex-col items-center justify-end p-5"
-          >
-            <button type="submit"
-              onClick={deleteAccount}
-              class="flex items-center justify-center rounded-full text-white font-display bg-red-500 w-full my-2 py-2 disabled:bg-red-300 hover:bg-red-600 transition duration-300"
-              >
-              Delete Account
-            </button>
-            <p
-              class="text-sm mt-2 text-red-500"
-            >
-              This action is irreversible.
-            </p>
-          </div>
-        </div>
-        <div
-          class="flex flex-col items-center justify-center w-full"
-        >
-          <h1
-            class="text-2xl font-bold py-5"
-          >
-            Active Sessions
-          </h1>
-          <div
-            class="flex flex-col items-center w-1/2"
-          >
-            {configStore.userSessions
-              ? (
-                <div
-                  class="flex flex-col items-center w-full"
+            {configStore.user && configStore.user.email_verified &&
+              <>
+                <span
+                  class="text-sm"
                 >
+                  |
+                </span>
+                <A
+                  href="#"
+                  onClick={sendVerificationEmail}
+                  class="text-sm text-indigo-500 hover:underline transition duration-300"
+                >
+                  Resend verification email
+                </A>
+              </>
+            }
+            <span
+              class="text-sm"
+            >
+              |
+            </span>
+            <A
+              href="#"
+              onClick={deleteAccount}
+              class="text-sm text-indigo-500 hover:underline transition duration-300"
+            >
+              Delete account
+            </A>
+          </div>
+        </div>
+      </div>
+      <div
+        class="flex flex-col items-center justify-center p-5 w-full max-w-4xl border-t"
+      >
+        <h1
+          class="text-xl font-bold py-5"
+        >
+          Access Logs
+        </h1>
+        <div
+          class="flex flex-col items-center w-3/4"
+        >
+          {configStore.userSessions
+            ? (
+              <div
+                class="flex flex-col items-center justify-center space-y-2 w-full max-w-2xl py-2"
+              >
+                <div
+                  class="flex items-center w-full justify-center"
+                >
+                  <label
+                    class="flex items-center w-full justify-center py-2 space-x-2"
+                  >
+                    <p
+                      class="font-bold w-40"
+                    >
+                      Filter by Date:
+                    </p>
+                    <input
+                      type="date"
+                      class="border border-gray-300 rounded-md w-full px-2"
+                      value={configStore.searchQuery}
+                      onInput={(e) => setConfigStore('searchQuery', e.currentTarget.value)}
+                    />
+                  </label>
+                </div>
+                <div
+                  class="grid grid-cols-2 space-y-2 items-center justify-center w-full"
+                >
+                  <div
+                    class="grid grid-cols-2 col-span-2 w-full border-b border-gray-300"
+                  >
+                    <p
+                      class="font-bold py-2"
+                    >
+                      Login At
+                    </p>
+                    <p
+                      class="font-bold py-2"
+                    >
+                      Logout At
+                    </p>
+                  </div>
                   <For
-                    each={configStore.userSessions}
+                    each={filteredUserSessions()}
                     fallback={
-                      <p
-                        class="text-center"
-                      >
-                        No active sessions
-                      </p>
+                      <>
+                        <p>
+                          No sessions found.
+                        </p>
+                      </>
                     }
                   >
                     {(session, index) => (
-                      <div
-                        class="grid grid-cols-7 items-center justify-center w-full border-b border-gray-300 py-2"
-                      >
-                        <div
-                          class="grid grid-cols-2 col-span-5 items-center justify-center w-full"
+                      <>
+                        <p
+                          class="-ml-4"
                         >
-                          <p>
-                            Logged in at: {new Date(session.login_at).toLocaleString()}
-                          </p>
-                          <p>
-                            Logged out at: {session.logout_at ? new Date(session.logout_at).toLocaleString() : "–"}
-                          </p>
-                        </div>
-                      </div>
+                          <b>{index() + 1}. </b>{new Date(session.login_at).toLocaleString()}
+                        </p>
+                        <p>
+                          {session.logout_at ? new Date(session.logout_at).toLocaleString() : "–"}
+                        </p>
+                      </>
                     )}
                   </For>
                 </div>
-              )
-              :
-              <p
-                class="text-center"
-              >
-                Loading...
-              </p>
-            }
-          </div>
+              </div>
+            )
+            :
+            <p
+              class="text-center"
+            >
+              Loading...
+            </p>
+          }
         </div>
       </div>
     </div>
